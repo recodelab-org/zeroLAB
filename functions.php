f04be76d2cf86b069df5a6c6adc06e6e9a3aefb8
Enno Hyttrek <ehy.global@gmail.com>
2020-12-21 21:12:54 +0100

    Initial commit

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+
+function my_style() {
+    wp_enqueue_style(  'style', get_stylesheet_uri()); 
+}
+add_action( 'wp_enqueue_scripts', 'my_style' );
+
+
+/*
+zeroLAB – a totally naked WordPress theme for you to start your theme development . From zero to hero …
+Copyright (C) 2020  Enno Hyttrek
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+https://www.gnu.org/licenses/
+*/
+?>
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--
+zeroLAB – a totally naked WordPress theme for you to start your theme development . From zero to hero …
+Copyright (C) 2020  Enno Hyttrek
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+https://www.gnu.org/licenses/
+-->
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <title><?php bloginfo('title'); ?></title>
+    <?php wp_head() ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+<p>Hello! I am just static text, hardcoded into the index.php HTML code. You need to replace and supplement me with the proper WP functions to make me dynamic …</p>
+
+</body>
+
+</html>
